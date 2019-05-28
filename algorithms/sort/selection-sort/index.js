a3258020d43456f94066e51fd5c517642de2b423
rocco <rocco.mormont@gmail.com>
2019-05-28 19:19:07 +0800

    :recycle: refactor selection-sort code

--- algorithms/sort/selection-sort/index.js
+++ algorithms/sort/selection-sort/index.js
@@ -1,11 +1,24 @@
 const { swap } = require('../../utils')
 
 module.exports = function selectionSort (arr = []) {
-  for (let outer = 0; outer < arr.length; outer++) {
+  let min
+  for (let outer = 0; outer < arr.length - 1; outer++) {
+    min = outer
     for (let inner = outer + 1; inner < arr.length; inner++) {
-      if (arr[outer] > arr[inner]) {
-        swap(arr, outer, inner)
+      if (arr[min] > arr[inner]) {
+        min = inner
       }
     }
+    swap(arr, outer, min)
   }
 }
+
+// module.exports = function selectionSort (arr = []) {
+//   for (let outer = 0; outer < arr.length; outer++) {
+//     for (let inner = outer + 1; inner < arr.length; inner++) {
+//       if (arr[outer] > arr[inner]) {
+//         swap(arr, outer, inner)
+//       }
+//     }
+//   }
+// }
