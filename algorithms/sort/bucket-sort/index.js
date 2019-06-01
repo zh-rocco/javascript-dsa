a95919c7a9bef40523c877cf311bd25bdb4e0345
rocco <rocco.mormont@gmail.com>
2019-06-01 17:50:34 +0800

    :sparkles: buckets sort support negative number

--- algorithms/sort/bucket-sort/index.js
+++ algorithms/sort/bucket-sort/index.js
@@ -1,31 +1,48 @@
 const { swap } = require('../../utils')
 
+function childSort (buckets, index, value) {
+  if (!buckets[index]) {
+    buckets[index] = []
+  }
+
+  buckets[index].push(value)
+
+  let len = buckets[index].length
+
+  while (len > 0) {
+    if (buckets[index][len] < buckets[index][len - 1]) {
+      swap(buckets[index], len, len - 1)
+    }
+    len--
+  }
+}
+
 module.exports = function bucketSort (arr = [], num = 2) {
   const max = Math.max(...arr)
   const min = Math.min(...arr)
-  const buckets = []
-  const bucketsSize = Math.ceil((max - min) / num)
+  const buckets = [] // positive buckets
+  const negativeBuckets = [] // negative buckets
+  const bucketsSize = Math.ceil((max - min) / num) || 1
 
   for (let i = 0; i < arr.length; i++) {
     const index = Math.floor(arr[i] / bucketsSize)
 
-    if (!buckets[index]) {
-      buckets[index] = []
-    }
-
-    buckets[index].push(arr[i])
-
-    let len = buckets[index].length
-
-    while (len > 0) {
-      if (buckets[index][len] < buckets[index][len - 1]) {
-        swap(buckets[index], len, len - 1)
-      }
-      len--
+    if (index < 0) {
+      childSort(negativeBuckets, Math.abs(index), arr[i])
+    } else {
+      childSort(buckets, index, arr[i])
     }
   }
 
+  console.log(buckets, negativeBuckets)
+
   const wrapBuckets = []
+
+  for (let i = negativeBuckets.length; i > -1; i--) {
+    if (negativeBuckets[i]) {
+      wrapBuckets.push(...negativeBuckets[i])
+    }
+  }
 
   for (let i = 0; i < buckets.length; i++) {
     if (buckets[i]) {
